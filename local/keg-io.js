d4deda0d645caabc35841e4ab673d3277fb2b156
Jay Taggart <jataggart@gmail.com>
2012-08-23 23:41:23 -0500

    implement close valve and first pass at the test routine

--- local/keg-io.js
+++ local/keg-io.js
@@ -9,7 +9,7 @@ var KegIO = (function() {
     var SerialPort = serialPort.SerialPort;
     var _port;
     var _emitter;
-    var fakePour = 0;
+    var debugFlow = false;
 
     function init(logger) {
         _emitter = Object.create(EventEmitter.prototype);
@@ -63,6 +63,12 @@ var KegIO = (function() {
         }
     }
 
+    function closeValve() {
+        if (!Config.isDebug) {
+            _port.write(protocol.REQUEST_CLOSE);
+        }
+    }
+
     function fakeTemp() {
         setInterval(function() {
             var temp = Math.random() * 10 + 35;
@@ -71,44 +77,42 @@ var KegIO = (function() {
     }
 
     function fakeTag() {
-        setInterval(function() {
-            var RFID = 'DENYTAG012';
-            if (Config.isAdminDebug) {
-                RFID = randomTag();
-            } else {
-                fakePour = 0;
-                var randomUser = Math.floor(Math.random() * 4);
-                switch (randomUser) {
-                    case 0:
-                        RFID = 'a123456789';
-                        break;
-                    case 1:
-                        RFID = '123456789a';
-                        break;
-                    case 2:
-                        RFID = '23456789a1';
-                        break;
-                    default:
-                        RFID = 'a123456789';
-                        break;
-                }
+        var RFID = 'DENYTAG012';
+        if (Config.isAdminDebug) {
+            RFID = randomTag();
+        } else {
+            var randomUser = Math.floor(Math.random() * 4);
+            switch (randomUser) {
+                case 0:
+                    RFID = 'a123456789';
+                    break;
+                case 1:
+                    RFID = '123456789a';
+                    break;
+                case 2:
+                    RFID = '23456789a1';
+                    break;
+                default:
+                    RFID = 'a123456789';
+                    break;
             }
-            parseMessage('**' + protocol.TAG + '_' + RFID + '**');
-        }, 25000);
-
+        }
+        parseMessage('**' + protocol.TAG + '_' + RFID + '**');
+        setTimeout(fakeTag, Math.random() * 20000);
     }
 
     function fakeFlow(timeLeft) {
-        var flow = Math.random() * 2;
-        fakePour = fakePour + flow;
-        parseMessage('**' + protocol.FLOW + '_' + flow + '**');
-        if (timeLeft >= 0) {
-            setTimeout(function() {
-                fakeFlow(timeLeft - 1);
-            }, 1000);
+        if(!debugFlow) {
+            debugFlow = true;
+            var flow = Math.random() * 2;
+            parseMessage('**' + protocol.FLOW + '_' + flow + '**');
+            if (timeLeft >= 0) {
+                setTimeout(function() {
+                    fakeFlow(timeLeft - 1);
+                }, 500);
+            }
         } else {
-            parseMessage('**' + protocol.FLOW + '_END**');
-            parseMessage('**' + protocol.POUR + '_' + fakePour + '**');
+            debugFlow = false;
         }
     }
 
@@ -125,7 +129,8 @@ var KegIO = (function() {
     return {
         getEmitter : getEmitter,
         start : init,
-        openValve : openValve
+        openValve : openValve,
+        closeValve : closeValve
     }
 }());
 module.exports = KegIO;
